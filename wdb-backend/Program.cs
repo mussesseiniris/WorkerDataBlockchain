f3231081171a474890fa01f54e54788a72094489
Iris <zhangsi1@myvuw.ac.nz>
2026-04-12 17:16:03 +1200

    chore: add Swagger UI for API documentation

--- wdb-backend/Program.cs
+++ wdb-backend/Program.cs
@@ -6,7 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+//Add Swagger service.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddControllers();
+
 
 builder.Services.AddDbContextPool<AppDbContext>(opt =>
     opt.UseNpgsql(
@@ -17,5 +22,9 @@ var app = builder.Build();
 app.MapControllers();
 app.MapOpenApi();
 
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.Run();
