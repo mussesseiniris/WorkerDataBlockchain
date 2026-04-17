e6e9dfe969d5300afa5fc6ebedb4f089d0190186
egantech05 <eganxhart@gmail.com>
2026-04-17 16:35:14 +1200

    creted contract in blockchain, created transaction module in blockchain. deployed the contract

---
+++ wdb-blockchain/contracts/TransactionLog.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract TransactionLog {
+  // order must match those in backend
+  enum Action {
+    PermissionRequested, // 0
+    PermissionApproved, // 1
+    PermissionRejected, // 2
+    DataViewed, // 3
+    PermissionRevoked // 4
+  }
+
+  // Transaction logs
+  // indexed fields can be filtered/searched efficiently
+  event TransactionLogged(
+    address indexed employer,
+    address indexed worker,
+    uint256 date,
+    Action action
+  );
+
+  // emits the log event
+  function logTransaction(
+    address employer,
+    address worker,
+    uint256 date,
+    Action action
+  ) external {
+    emit TransactionLogged(employer, worker, date, action);
+  }
+}
