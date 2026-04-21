706a44de7bf2c2a8c723db0996f33c53d9f3a4f2
alyan <gierraalya@myvuw.ac.nz>
2026-04-21 12:38:50 +1200

    test: integration test for PermissionRepo GetAllByWorkerId & RequestRepo GetAllByRequestId; refactored GetAllByRequestId return type to one item instead of list; refactored unit tests due to the return type change

--- wdb-backend/Services/RequestRepoImpl.cs
+++ wdb-backend/Services/RequestRepoImpl.cs
@@ -1,10 +1,17 @@
 using wdb_backend.Abstractions;
+using wdb_backend.Data;
 using wdb_backend.Models;
 
 namespace wdb_backend.Services;
 
 public class RequestRepoImpl : IRequestRepository
 {
+    private readonly AppDbContext _dbContext;
+
+    public RequestRepoImpl (AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
     public Task AddAsync(Guid employerId, Guid workerId, Request request, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
