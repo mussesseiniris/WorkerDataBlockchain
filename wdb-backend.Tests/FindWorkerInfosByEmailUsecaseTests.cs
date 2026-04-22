6605efd84ee6ec08e9d9cecfb9aa0d7861c46fd3
Iris <zhangsi1@myvuw.ac.nz>
2026-04-22 14:25:43 +1200

    test: add tests for FindWorkerInfosByEmailUsecaseImpl FindWorkerInfosByEmail

---
+++ wdb-backend.Tests/FindWorkerInfosByEmailUsecaseTests.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using Moq;
+using wdb_backend.Abstractions;
+using wdb_backend.Models;
+using wdb_backend.Usecases;
+
+
+public class FindWorkerInfosByEmailUsecaseTests
+{
+    [Fact]
+    public async Task FindWorkerInfosByEmail()
+    {
+        // Arrange
+        var mockWorkerService = new Mock<IWorkerService>();
+        var mockWorkerInfoService = new Mock<IWorkerInfoService>();
+        var findUsecase = new FindWorkerInfosByEmailUsecaseImpl(mockWorkerService.Object, mockWorkerInfoService.Object);
+        var fakeWorker = new Worker { Id = Guid.NewGuid(), Email = "test@email", Name = "test" };
+        var worker_info1 = new WorkerInfo { Desc = "address", Value = "havana rise" };
+        var worker_info2 = new WorkerInfo { Desc = "phone", Value = "123456" };
+        var workerInfos = new List<WorkerInfo>();
+        workerInfos.Add(worker_info1);
+        workerInfos.Add(worker_info2);
+        mockWorkerService.Setup(r => r.GetByEmailAsync(fakeWorker.Email)).ReturnsAsync(fakeWorker);
+        mockWorkerInfoService.Setup(r => r.GetAllAsync(fakeWorker.Id)).ReturnsAsync(workerInfos);
+        // Act
+        var resultWorkerInfos = await findUsecase.FindWorkerInfosByEmail(fakeWorker.Email, default);
+        // Assert
+        Assert.Equivalent(workerInfos, resultWorkerInfos);
+    }
+    
+}
