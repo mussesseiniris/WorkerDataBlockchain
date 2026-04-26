ee0998518494129ae4ec18c7e96ea87ccfae5be1
Iris <zhangsi1@myvuw.ac.nz>
2026-04-27 08:38:04 +1200

    feat: implement workerRepoImpl GetByEmailAsync

--- wdb-backend/Services/WorkerRepoImpl.cs
+++ wdb-backend/Services/WorkerRepoImpl.cs
@@ -1,10 +1,20 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 using wdb_backend.Abstractions;
+using wdb_backend.Data;
 using wdb_backend.Models;
 
 namespace wdb_backend.Services;
 
-public class WorkerRepoImpl:IWorkerRepository
+public class WorkerRepoImpl : IWorkerRepository
 {
+    private readonly AppDbContext _context;
+
+    public WorkerRepoImpl(AppDbContext context)
+    {
+        _context = context;
+    }
+
     public Task AddOneAsync(Guid workerId, WorkerInfo workerInfo, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
@@ -15,9 +25,15 @@ public class WorkerRepoImpl:IWorkerRepository
         throw new NotImplementedException();
     }
 
-    Task<Worker> IWorkerRepository.GetByEmailAsync(string email, CancellationToken cancellationToken)
+    public async Task<Worker> GetByEmailAsync(string email, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var resultWorker = await _context.Workers.FirstOrDefaultAsync(w => w.Email == email, cancellationToken);
+        if (resultWorker == null)
+        {
+            throw new KeyNotFoundException($"Worker with {email} email not found");
+        }
+
+        return resultWorker;
     }
 
     public Task AddAsync(Worker worker, CancellationToken cancellationToken = default)
@@ -34,5 +50,4 @@ public class WorkerRepoImpl:IWorkerRepository
     {
         throw new NotImplementedException();
     }
-
 }
