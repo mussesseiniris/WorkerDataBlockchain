722998b2b6d1c796eee291cd6a14cdc5eb06cece
alyan <gierraalya@myvuw.ac.nz>
2026-04-28 22:39:52 +1200

    feat: implement tabs for active request and active access frontend

--- wdb-frontend/app/worker/requests/page.tsx
+++ wdb-frontend/app/worker/requests/page.tsx
@@ -1,2 +1,53 @@
 // Worker requests: view, approve, or reject employer data access requests
-export default function Page() { return null }
+// import { FetchApi } from '@lib/api';
+import { useState, ReactNode } from 'react';
+
+interface TabProps {
+    id: string;
+    label: string;
+    children?: ReactNode;
+}
+
+const tabs: TabProps[] = [
+    {
+        id: "Active request",
+        label: "active request",
+        children: 
+        <div>
+            <p>
+                List of permission requests
+            </p>  
+        </div>
+    },
+    {
+        id: "Active access",
+        label: "active access"
+    }
+]
+
+
+
+export default function Page() { 
+    
+   const [activeTab, setActiveTab] = useState<string>(tabs[0].id);
+   const activeContent = tabs.find((t) => t.id == activeTab)?.children;
+    
+    
+    return (
+        <main>
+            <div>
+                {tabs.map(({id, label}) => 
+                <button 
+                key={id}
+                aria-selected={activeTab === id}
+                onClick={() => setActiveTab(id)}
+                >
+                </button>
+                )}
+            </div>
+            <div>
+                {activeContent}
+            </div>
+        </main>
+        
+    ) }
