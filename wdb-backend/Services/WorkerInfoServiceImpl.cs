19c433061f4f48bf4be97e6b7c2fac1f614eb5f7
alyan <gierraalya@myvuw.ac.nz>
2026-04-28 14:17:41 +1200

    feat: implement WorkerInfoService & WorkerInfoRepo GetAll; implement controller for retreiving all workerinfo

--- wdb-backend/Services/WorkerInfoServiceImpl.cs
+++ wdb-backend/Services/WorkerInfoServiceImpl.cs
@@ -5,14 +5,20 @@ namespace wdb_backend.Services;
 
 public class WorkerInfoServiceImpl:IWorkerInfoService
 {
+    private readonly IWorkerInfoRepository _workerInfoRepo;
+    public WorkerInfoServiceImpl(IWorkerInfoRepository workerInfoRepo)
+    {
+        _workerInfoRepo = workerInfoRepo;
+    }
     public Task<WorkerInfo> GetOneAsync(Guid workerId, Guid workerInfoId, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
     }
 
-    public Task<HashSet<WorkerInfo>> GetAllAsync(Guid workerId, CancellationToken cancellationToken = default)
+    public async Task<List<WorkerInfo>> GetAllAsync(Guid workerId, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var resultInfos = await _workerInfoRepo.GetAllAsync(workerId,default)??throw new KeyNotFoundException();
+        return resultInfos;
     }
 
     public Task<WorkerInfo> CreateAsync(Guid workerId, WorkerInfo workerInfo, CancellationToken cancellationToken = default)
