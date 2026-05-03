c9fdf5e7203ea48e34b0aa16861bf135ec727b6b
alyan <gierraalya@myvuw.ac.nz>
2026-05-03 23:07:46 +1200

    feat: implement remove request when approved/rejected from list

--- wdb-frontend/app/worker/requests/ActiveRequestTab.tsx
+++ wdb-frontend/app/worker/requests/ActiveRequestTab.tsx
@@ -1,19 +1,27 @@
 "use client";
 
 import RequestRow, { Row, Field } from "../../components/RequestRow"
+import { useState } from "react";
 
 interface RowList {
     requests: Row[]
 }
+
 export default function RequestRowTab({requests}: RowList){
+    const [rows, setRows] = useState<Row[]>(requests); 
+    
+    const handleComplete = (id: string) => {
+    setRows((prev) => prev.filter((r) => r.id !== id));
+    };
+    
     if (requests.length === 0) {
         return <p className="text-sm text-gray-500">No active permission requests</p>;
     }
 
      return (
         <div>
-            {requests.map((item) => (
-                <RequestRow key={item.id} {...item} />
+            {rows.map((item) => (
+                <RequestRow key={item.id} {...item} onComplete={handleComplete}/>
             ))}
         </div>
     );
