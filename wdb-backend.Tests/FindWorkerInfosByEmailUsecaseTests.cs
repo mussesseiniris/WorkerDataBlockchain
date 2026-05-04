4115eb781ad1719e785bf61c03712b1db4c7367e
Iris <zhangsi1@myvuw.ac.nz>
2026-05-04 17:40:15 +1200

    test: change GetAllAsync to GetAllAsyncList of WorkerInfo Service in tests

--- wdb-backend.Tests/FindWorkerInfosByEmailUsecaseTests.cs
+++ wdb-backend.Tests/FindWorkerInfosByEmailUsecaseTests.cs
@@ -37,13 +37,7 @@ public class FindWorkerInfosByEmailUsecaseTests
         var mockWorkerInfoService = new Mock<IWorkerInfoService>();
         var findUsecase = new FindWorkerInfosByEmailUsecaseImpl(mockWorkerService.Object, mockWorkerInfoService.Object);
         var fakeWorker = new Worker { Id = Guid.NewGuid(), Email = "test@email", Name = "test" };
-        // var worker_info1 = new WorkerInfo { Desc = "address", Value = "havana rise" };
-        // var worker_info2 = new WorkerInfo { Desc = "phone", Value = "123456" };
-        // var workerInfos = new List<WorkerInfo>();
-        // workerInfos.Add(worker_info1);
-        // workerInfos.Add(worker_info2);
-        // mockWorkerService.Setup(r => r.GetByEmailAsync(fakeWorker.Email)).ReturnsAsync(fakeWorker);
-        // mockWorkerInfoService.Setup(r => r.GetAllAsync(fakeWorker.Id)).ReturnsAsync(workerInfos);
+    
         // Act
         var resultWorkerInfos = await findUsecase.FindWorkerInfosByEmail(fakeWorker.Email, default);
         // Assert
