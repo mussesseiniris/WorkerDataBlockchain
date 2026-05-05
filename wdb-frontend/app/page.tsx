3b06b6577ee058f1f749fa855d694fe74308dc1c
Iris <zhangsi1@myvuw.ac.nz>
2026-05-05 13:53:51 +1200

    feat: redirect to dashboard after login and redirect landing page to log in page

--- wdb-frontend/app/page.tsx
+++ wdb-frontend/app/page.tsx
@@ -1,16 +1,21 @@
+import { redirect } from 'next/navigation';
 
 /*Home page: landing page with project introduction
 * @author:
 * @since:
 * @version:
 */
-export default function Home() {
+// export default function Home() {
   
-  return (
-    <div className="text-center bg-blue-80 min-h-screen">
-      <h1 className="text-4xl font-bold mt-32">Worker Data Blockchain</h1>
-      <p className="text-xl font-bold mt-12 text-violet-300">Hi team! Welcome to Worker Data Blockchain. 
-        <br />Excited to build this together with you all — let's make it happen!</p>
-    </div>
-  );
-}
+//   // return (
+//   //   <div className="text-center bg-blue-80 min-h-screen">
+//   //     <h1 className="text-4xl font-bold mt-32">Worker Data Blockchain</h1>
+//   //     <p className="text-xl font-bold mt-12 text-violet-300">Hi team! Welcome to Worker Data Blockchain. 
+//   //       <br />Excited to build this together with you all — let's make it happen!</p>
+//   //   </div>
+//   // );
+//   redirect
+// }
+export default function Home() {
+    redirect('/login');
+  }
