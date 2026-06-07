156bab10cdc00013a6afd72e375624dafd2481a9
Will Cao <willcao0101@gmail.com>
2026-06-07 22:19:12 +1200

    Add Sepolia network to Hardhat config

--- wdb-blockchain/hardhat.config.ts
+++ wdb-blockchain/hardhat.config.ts
@@ -1,11 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_URL ?? "",
+      accounts: process.env.DEPLOYER_PRIVATE_KEY
+        ? [process.env.DEPLOYER_PRIVATE_KEY]
+        : []
     }
   }
 };
